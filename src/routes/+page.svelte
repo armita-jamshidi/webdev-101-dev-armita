b88a346fb83c8c1e07b1fee7f29cce30c792d6a8
armita-jamshidi <aaj34@cornell.edu>
2023-02-19 16:21:53 -0500

    Conditionally render create post button

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,18 @@
-<button class="btn btn-primary">Create Post</button>
+<script>
+	import { currentUser } from '$lib/pocketbase';
+</script>
 
-<progress class="progress progress-primary w-56" value="10" max="100"></progress>
+{#if $currentUser}
+	<div class="flex justify-center">
+		<!-- The button to open modal -->
+		<label for="my-modal-4" class="btn-primary btn">Create Post</label>
+
+		<!-- Put this part before </body> tag -->
+		<input type="checkbox" id="my-modal-4" class="modal-toggle" />
+		<label for="my-modal-4" class="modal cursor-pointer">
+			<label class="modal-box relative" for="">
+				<h3 class="text-lg font-bold">Create Post</h3>
+			</label>
+		</label>
+	</div>
+{/if}
